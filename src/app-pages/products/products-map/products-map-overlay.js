cbbaf0fbace1d082e4098e85a95a9b56d2d97696
Jordan Van Aalsburg <jordan.vanaalsburg@usace.army.mil>
2025-01-16 09:42:15 -0800

    Add button for uploading Shapefiles

--- src/app-pages/products/products-map/products-map-overlay.js
+++ src/app-pages/products/products-map/products-map-overlay.js
@@ -7,13 +7,20 @@ import {
   TerraDrawSelectMode,
 } from 'terra-draw';
 import { TerraDrawLeafletAdapter } from 'terra-draw-leaflet-adapter';
+import shp from 'shpjs';
 
 const trashIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-2" style="scale:0.7">
   <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
 </svg>`;
 
+const uploadIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-2" style="scale:0.7">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
+</svg>`;
+
 export default function ProductsMapOverlay(props) {
   const draw = useRef(null);
+  const layerGroup = useRef(null);
+  const fileInput = useRef(null);
   const map = useMap();
 
   useEffect(() => {
@@ -46,6 +53,9 @@ export default function ProductsMapOverlay(props) {
     terraDraw.on('finish', onCreate);
 
     Leaflet.easyButton(trashIcon, onDelete).addTo(map);
+    Leaflet.easyButton(uploadIcon, async () =>
+      fileInput.current?.click(),
+    ).addTo(map);
 
     draw.current = terraDraw;
   }, []);
@@ -61,10 +71,35 @@ export default function ProductsMapOverlay(props) {
   };
 
   const onDelete = () => {
+    if (layerGroup.current) {
+      map.removeLayer(layerGroup.current);
+      layerGroup.current = null;
+    }
+
     draw.current.clear();
     draw.current.setMode('rectangle');
     props.onRegionUpdate(null);
   };
 
-  return null;
+  const onFileUpload = async (e) => {
+    const file = e.target.files[0];
+
+    const data = await file.arrayBuffer();
+    const geojson = await shp(data);
+
+    const group = new Leaflet.LayerGroup();
+    group.addLayer(Leaflet.geoJSON(geojson));
+    group.addTo(map);
+
+    layerGroup.current = group;
+  };
+
+  return (
+    <input
+      type='file'
+      ref={fileInput}
+      onChange={onFileUpload}
+      style={{ display: 'none' }}
+    />
+  );
 }
